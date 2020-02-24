60cfeed35fa1eb3d1d1ad3be2d219a82dfdc0f35
Manu M Bhat <46175054+ManuMBhat@users.noreply.github.com>
2020-02-24 09:57:48 +0530

    TopoSort
    Works on adjacency matrix

---
+++ Open_Ended/OpenEnded(Toposort).c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int* topoSort(int n,int graph[][n]){
+    int source  = -1;
+    int k = 0;
+    int* ans = (int*)malloc(sizeof(int)*n);
+    while(k<n){
+        int bigFlag = 1;
+        
+        for(int i=0;i<n && bigFlag;i++){
+            int flag = 0;
+            for(int j=0;j<n;j++){
+                if(j!=i && graph[i][j] == 1){
+                    flag = 1;
+                }
+            }
+            if(flag == 0){
+                source = i;
+                bigFlag = 0;
+            }
+        }
+        if(flag == 1){
+            printf("DAG Found");
+            for(int i=0;i<n;i++){
+                ans[i] = -1;
+            }
+            return ans;
+        }
+        ans[k] = source;
+        k++;
+        for(int i=0;i<n;i++){
+            graph[i][source] = 0;
+        }
+    }
+    return ans;
+
+
+}
